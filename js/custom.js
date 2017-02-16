cc9eafcfd35e92460f05a662eac639ba63d16466
Wood71 <gullandrea693@gmail.com>
2017-02-16 22:11:15 +0100

    Update

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var c = 0;
+$(document).ready(function(){/*Handler jquery click project*/
+    $(".mfp-arrow-right").focus(function(){
+    	console.log("ok");
+    	//if (c <= 1)
+ 		//	c++;
+ 		//else
+ 		//	location.href='index.html';
+	});
+});
